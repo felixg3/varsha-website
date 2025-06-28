9e9d983d587a4706b38dfa80e67e285118d83fd6
Felix Gießmann <felix@giessmann.net>
2025-06-28 17:16:00 +0200

    chore: remove legacy static files and regenerate dist

--- main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const toggle = document.getElementById('darkToggle');
-  toggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
-  });
-});
